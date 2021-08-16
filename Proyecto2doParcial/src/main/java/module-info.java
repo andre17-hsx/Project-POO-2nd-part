80050002e967dc7deed59246a7e1c0576ea8e029
Andre Aguirre <andaapol@espol.edu.ec>
2021-08-15 22:59:45 -0500

    se creo el proyecto y el repositorio compartido para poder empezar a trabajar en paralelo

--- Proyecto2doParcial/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.proyecto2doparcial {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.proyecto2doparcial to javafx.fxml;
-    exports com.mycompany.proyecto2doparcial;
-}
